c38e10f31809af1392f7febf7ed80d1f614b3dd4
girishhh <girikulkarni03@gmail.com>
2017-04-25 18:53:29 +0530

    chnages

--- src/app/time-news.service/time-news.service.ts
+++ src/app/time-news.service/time-news.service.ts
@@ -5,14 +5,13 @@ import {Http,Headers} from '@angular/http';
 
 
 @Injectable()
-export class TimeNewsService{
-   private api_key = "8dd8bcb1d3014845a82a7a2bb1d23d7f";
-   private url = "https://newsapi.org/v1/articles?source=the-times-of-india&sortBy=top&apiKey=" + this.api_key;          
+export class TimeNewsService{   
+   private url = "http://kre-feed_dev.kreatio.info/?q=http://feeds.bbci.co.uk/news/world/rss.xml";          
 
    constructor(private http: Http){}
    
 
    getLatestArticle(): Observable<any>{                              
-       return this.http.get(this.url).map(response=>response.json().articles[0] as any);
+       return this.http.get(this.url).map(response=>response.json().responseData.feed.entries[0] as any)
    }
 }
--- src/app/times-news-twitter.service/times-news-twitter.service.ts
+++ src/app/times-news-twitter.service/times-news-twitter.service.ts
@@ -8,11 +8,7 @@ export class TimesNewsTwitterService{
     constructor(private http: Http){}
     url= "http://kre-feed.mailfixx.com/?q=https://twitrss.me/twitter_user_to_rss/?user=kreatio_wcm";
 
-    getLatestArticle(): Observable<any>{
-        return this.http.get(this.url).map(response=> { 
-            if(response.json().responseData.feed) {
-                response.json().responseData.feed.entries[0] as any
-            }            
-        } );
+    getLatestArticle(): Observable<any>{        
+        return this.http.get(this.url).map(response=>response.json().responseData.feed.entries[0] as any);        
     }
 }
