918cd2aaedf66effc6026032b8529467d121b12e
girishhh <girikulkarni03@gmail.com>
2017-04-27 18:38:51 +0530

    timer added

--- src/app/widgets.component/widgets.component.ts
+++ src/app/widgets.component/widgets.component.ts
@@ -7,6 +7,8 @@ import {ReadabilityScoreService} from '../readability-score.service/readability-
 import {ReadabilityScoreSharedService} from '../readability-score-shared.service/readability-score-shared.service';
 import {Observable} from 'rxjs/Observable';
 import {Subscription} from 'rxjs/Subscription';
+import {TimerObservable} from "rxjs/observable/TimerObservable";
+
 
 
 @Component({
@@ -25,16 +27,41 @@ export class WidgetsComponent {
     constructor(private readability_shared_service: ReadabilityScoreSharedService,private readability_score_service: ReadabilityScoreService,private time_news_service: TimeNewsService,private earth_quak_news_service: EarthQuakNewsService,private times_twitter_service: TimesNewsTwitterService,private bse_sensex_service: BseSensexNewsService,private change_detector: ChangeDetectorRef){        
     }
 
-    ngOnInit(){        
-        this.time_news_service.getLatestArticle().subscribe(articles => {this.hindustan_news_articles = articles});                
-        this.earth_quak_news_service.getLatestArticle().subscribe(articles=>{this.earth_quak_articles=articles;});
-        this.times_twitter_service.getLatestArticle().subscribe(articles=>{this.times_twitter_articles = articles;});
-        this.bse_sensex_service.geLatestArticle().subscribe(articles=>{this.bse_articles=articles;});            
-
+    ngOnInit(){                         
         this.readabiity_score_subscription = this.readability_shared_service.notifyObservable$.subscribe((text) => {            
             this.readability_score_service.getReadabilityScore(text).subscribe(readability_score=>{this.readabilityscore = readability_score;}) 
+        });        
+
+        let timer = TimerObservable.create(0, 10000);
+
+        timer.subscribe(t => {
+            this.fetch_bse_articles();
+            this.fetch_times_news_articles();
+            this.fetch_earth_quik_articles();
+            this.fetch_times_twitter_articles();
         });
+
+        
     }
+
+    fetch_times_twitter_articles(){
+        this.times_twitter_service.getLatestArticle().subscribe(articles=>{this.times_twitter_articles = articles;});
+    }
+
+
+    fetch_earth_quik_articles(){
+        this.earth_quak_news_service.getLatestArticle().subscribe(articles=>{this.earth_quak_articles=articles;});
+    }
+
+
+    fetch_times_news_articles(){
+        this.time_news_service.getLatestArticle().subscribe(articles => {this.hindustan_news_articles = articles});                
+    }
+
+    fetch_bse_articles(){        
+        this.bse_sensex_service.geLatestArticle().subscribe(articles=>{this.bse_articles=articles;});            
+    }
+    
 
      ngOnDestroy() {
         this.readabiity_score_subscription.unsubscribe();
